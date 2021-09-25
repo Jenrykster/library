a0d1447e406d146686e3979a73d8ef8792a6ff39
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-25 15:54:16 -0300

    Finish base layout

--- js/library.js
+++ js/library.js
@@ -11,3 +11,14 @@ function addBook(title, author, pages, hasRead){
     let newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 }
+
+
+let bookGrid = document.querySelector('#book-grid');
+let bookCard = document.createElement('div');
+let cardTemplate = '<div class="book-card"> <h3>Book Title</h3> <h4>Author</h4> <div> <p>777 pages</p> <label for="read">Read</label> <input name="read" type="checkbox" checked> </div> </div>';
+bookCard.innerHTML = cardTemplate.trim();
+
+
+for(let i=0; i<10;i++){
+    bookGrid.appendChild(bookCard.cloneNode(true));
+}
