482f9f5ab0518a74bf3593e458f9e3867f1fbbe7
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-27 12:30:42 -0300

    Add function to loop trough book array

--- js/library.js
+++ js/library.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-
 function Book(title, author, pages, hasRead){
     this.title = title;
     this.author = author;
@@ -12,13 +11,39 @@ function addBook(title, author, pages, hasRead){
     myLibrary.push(newBook);
 }
 
+let addButton = document.querySelector('#add-button');
+let form = document.querySelector('#register');
+
+addButton.onclick = function(){
+    let bookData = Object.fromEntries(new FormData(form).entries());
+    addBook(bookData.title, bookData.author, bookData.pages, bookData.read == 'on');
+    updateLib();
+}
+
+
 
 let bookGrid = document.querySelector('#book-grid');
-let bookCard = document.createElement('div');
-let cardTemplate = '<div class="book-card"> <h3>Book Title</h3> <h4>Author</h4> <div> <p>777 pages</p> <label for="read">Read</label> <input name="read" type="checkbox" checked> </div> </div>';
-bookCard.innerHTML = cardTemplate.trim();
 
-
-for(let i=0; i<10;i++){
-    bookGrid.appendChild(bookCard.cloneNode(true));
+function updateLib(){
+    let cards = document.querySelectorAll('.book-card[data-index]');
+    let indexes = [];
+    cards.forEach((bookCard => {
+        indexes.push(Number(bookCard.dataset.index));
+    }))
+    for(let book of myLibrary){
+        if(indexes.includes(myLibrary.indexOf(book))){  //Check if the same entry was already added 
+            return;
+        }
+        else{
+            let newBookCard = document.createElement('div');
+            let cardHTMLInfo = `<div class="book-card" data-index=${myLibrary.indexOf(book)}> <h3>${book.title}</h3> <h4>${book.author}</h4> <div> <p>${book.pages} pages</p> <label for="read">Read</label> <input name="read" class="hasread" type="checkbox"> </div> </div>`;
+            newBookCard.innerHTML = cardHTMLInfo.trim();
+            if(book.hasRead){
+                newBookCard.querySelector('input[type=checkbox]').checked = true;
+            }
+            bookGrid.appendChild(newBookCard);
+        }
+    }
 }
+
+window.onload = updateLib;
