5c7f6f46b39db03904efd42f582df08e35a3e5a4
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-28 00:49:26 -0300

    Add function to change read status

--- js/library.js
+++ js/library.js
@@ -28,7 +28,7 @@ function updateLib(){
     bookGrid.innerHTML = '';
     for(let book of myLibrary){
         let newBookCard = document.createElement('div');
-        let cardHTMLInfo = `<div class="book-card" data-index=${myLibrary.indexOf(book)}> <h3>${book.title}</h3> <h4>${book.author}</h4> <div> <p>${book.pages} pages</p> <label for="read">Read</label> <input name="read" class="hasread" type="checkbox"> </div><button onclick="removeCard(this)">remove</button> </div>`;
+        let cardHTMLInfo = `<div class="book-card" data-index=${myLibrary.indexOf(book)}> <h3>${book.title}</h3> <h4>${book.author}</h4> <div> <p>${book.pages} pages</p> <label for="read">Read</label> <input name="read" class="hasread" type="checkbox" onclick="changeStatus(this)"> </div><button onclick="removeCard(this)">remove</button> </div>`;
         newBookCard.innerHTML = cardHTMLInfo.trim();
         if(book.hasRead){
             newBookCard.querySelector('input[type=checkbox]').checked = true;
@@ -36,7 +36,15 @@ function updateLib(){
         bookGrid.appendChild(newBookCard);
     }
 }
-
+function changeStatus(e){
+    let cardIndex = e.parentNode.parentNode.dataset.index;
+    if(e.checked){
+        myLibrary[cardIndex].hasRead = true;
+    }else{
+        myLibrary[cardIndex].hasRead = false;
+    }
+    updateLib();
+}
 function removeCard(e){
     let index = e.parentNode.dataset.index;
     console.log(index);
