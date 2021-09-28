cc3e5a0b07ed7ae2bc3fc7e9fda1f033119472da
João Henrique Santana <joaoenrique.san@gmail.com>
2021-09-28 00:14:34 -0300

    Remove unnecessary condition on updateLib

--- js/library.js
+++ js/library.js
@@ -25,24 +25,15 @@ addButton.onclick = function(){
 let bookGrid = document.querySelector('#book-grid');
 
 function updateLib(){
-    let cards = document.querySelectorAll('.book-card[data-index]');
-    let indexes = [];
-    cards.forEach((bookCard => {
-        indexes.push(Number(bookCard.dataset.index));
-    }))
+    bookGrid.innerHTML = '';
     for(let book of myLibrary){
-        if(indexes.includes(myLibrary.indexOf(book))){  //Check if the same entry was already added 
-            return;
+        let newBookCard = document.createElement('div');
+        let cardHTMLInfo = `<div class="book-card" data-index=${myLibrary.indexOf(book)}> <h3>${book.title}</h3> <h4>${book.author}</h4> <div> <p>${book.pages} pages</p> <label for="read">Read</label> <input name="read" class="hasread" type="checkbox"> </div> </div>`;
+        newBookCard.innerHTML = cardHTMLInfo.trim();
+        if(book.hasRead){
+            newBookCard.querySelector('input[type=checkbox]').checked = true;
         }
-        else{
-            let newBookCard = document.createElement('div');
-            let cardHTMLInfo = `<div class="book-card" data-index=${myLibrary.indexOf(book)}> <h3>${book.title}</h3> <h4>${book.author}</h4> <div> <p>${book.pages} pages</p> <label for="read">Read</label> <input name="read" class="hasread" type="checkbox"> </div> </div>`;
-            newBookCard.innerHTML = cardHTMLInfo.trim();
-            if(book.hasRead){
-                newBookCard.querySelector('input[type=checkbox]').checked = true;
-            }
-            bookGrid.appendChild(newBookCard);
-        }
+        bookGrid.appendChild(newBookCard);
     }
 }
 
